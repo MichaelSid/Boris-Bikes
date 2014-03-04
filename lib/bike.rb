feefcf01c2a2bd1eabc48e31b3cc13927853ef00
Michael <michael.sidon@gmail.com>
2014-03-04 18:54:09 +0000

    Implemented boken? method

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,4 +1,17 @@
 class Bike
+
+	#the initialize method is always called when you create a new class by typing Bike.new
+	def initialize 
+		#all instance variables begin with "@"
+		#this must be an instance variable because we'll need it in other methods
+		@broken = false
+	end
+
 	def broken?
+		@broken
+	end
+
+	def break
+		@broken = true	
 	end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,11 +5,18 @@ require_relative "../lib/bike"
 #We're desribing the functionality of a specific class, Bike
 
 describe Bike do
+
 	#this is a specific feature (behaviour)
 	#that we expect to be present
 	it "should not be broken after we create it" do
 		bike = Bike.new #initialize a new object of Bike class
 		#expect an instance of this class to have a method "broken?" that should return false
 		expect(bike).not_to be_broken # can be rewritten as ==> expect(bike.broken?).to be_false
+	end
+
+	it "should be able to break" do
+		bike = Bike.new
+		bike.break
+		expect(bike).to be_broken
 	end
 end
