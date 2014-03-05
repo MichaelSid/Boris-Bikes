dff191dc258ad676f8e23bdf216d2b61d91086dc
Michael <michael.sidon@gmail.com>
2014-03-05 16:44:28 +0000

    Raise error on docking if full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,10 +2,13 @@ require_relative '../lib/docking_station.rb'
 
 describe DockingStation do
 
+	def fill_station(station)
+		20.times { station.dock(Bike.new)}
+	end
+
 	let(:bike) {Bike.new}
 	let(:station) {DockingStation.new}
 	let(:station) {DockingStation.new(:capacity => 20)}
-
 
 	it "should accept a bike" do
 		# we expect the station to have 0 bikes
@@ -24,8 +27,13 @@ describe DockingStation do
 
 	it "should know when it's full" do
 		expect(station).not_to be_full
-		20.times { station.dock(Bike.new) }
+		fill_station(station)
 		expect(station).to be_full
+	end
+
+	it "should not accept a bike if it's full" do
+		fill_station(station)
+		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
 	end
 
 end
